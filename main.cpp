050b5e81f184d1979cdf6c20c15d855544df630f
wzyzsy <2579351233@qq.com>
2025-06-03 18:30:48 +0800

    first commit

---
+++ main.cpp
@@ -0,0 +1,24 @@
+#include"LexicalAnalyser.h"
+#include "ParserAndSemanticAnalyser.h"
+#include "ObjectCodeGenerator.h"
+int main() {
+
+	
+	ParserAndSemanticAnalyser parserAndSemanticAnalyser("productions.txt");
+	parserAndSemanticAnalyser.outputDFA("DFA.txt");
+	//parserAndSemanticAnalyser.analyse(lexicalAnalyser.getResult(), "SLR1_analyse.txt");
+	////parserAndSemanticAnalyser.outputIntermediateCode();
+	//
+	//IntermediateCode* code = parserAndSemanticAnalyser.getIntermediateCode();
+	//code->divideBlocks(parserAndSemanticAnalyser.getFuncEnter());
+	////code->outputBlocks();
+
+	//ObjectCodeGenerator objectCodeGenerator;
+	//objectCodeGenerator.analyseBlock(code->getFuncBlock());
+	//objectCodeGenerator.outputIBlocks();
+	//objectCodeGenerator.generateCode();
+	//objectCodeGenerator.outputObjectCode();
+	//objectCodeGenerator.outputObjectCode("program.asm");
+
+	return 0;
+}
